e5ec5a6a9b25dd457167a20fe76070c9a9a62f86
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2020-09-10 12:02:49 +0200

    Added time iterator

--- Helpers.py
+++ Helpers.py
@@ -1,6 +1,12 @@
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 
 #Converts date to datetime format:  24-09-2020 -> 24-09-2020 00:00:00
 def date2datetime(date):
     t = time() #Returns time 00:00:00
     return(datetime.combine(date, t))
+
+
+#Date iterator: used to loop through intervals of time -> for t in daterange(start, end): doSomething 
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
