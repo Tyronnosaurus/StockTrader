0d0e5c923d7161ea715238b001fde7b6ab45dcca
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2020-09-14 12:16:43 +0200

    Repaired and improved Yahoo data download

--- FinancialData/GetStockData.py
+++ FinancialData/GetStockData.py
@@ -2,38 +2,50 @@ import os
 import sqlite3
 from GetTickersListFromIndex import GetTickersListFromIndex
 import yfinance as yf
+from SqlHelpers import ListToSqlColumn
 
 
+#Indexes to include. Yahoo Finances uses non-standard names for the indexes, and also appends an ^ preffix
+Indexes = ['^GDAXI',    #DAX
+           '^MDAXI',    #MDAX
+           '^IBEX']     #IBEX 35
 
-#Given an index of companies, downloads to a database the share price history of each indexed company
+
+#Given an index of companies, downloads to a database the share price history of the index and each individual company
 def DownloadIndexHistoricData(index):
     ListOfTickers = GetTickersListFromIndex(index)   #Obtain a list of companies' tickers
 
+    #Step 1: Create a list of the stocks in this index -> Table INDEXNAME_Stocks
+    cur.execute("CREATE TABLE IF NOT EXISTS '%s' (Stocks varchar(20))" % (index + "_Stocks"))
+    ListToSqlColumn(cur, '"' + index + '_Stocks"', ListOfTickers)
+
+    #Step 2: Create a price history of the index -> Table INDEXNAME
+    DownloadPriceHistory(index)
+
+    #Step 3: Create a price history of every stock  -> Tables STOCK1, STOCK2...
     for ticker in ListOfTickers:
-        DownloadStockHistoricData(ticker)
+        DownloadPriceHistory(ticker)
 
 
-#Given a company's ticker (i.e. its trading symbol), downloads to a database the share price history
-def DownloadStockHistoricData(ticker):
+
+#Download price history of an index or stock
+def DownloadPriceHistory(ticker):
         data = yf.Ticker(ticker)
-        #print(stock.info) #Caution: Causes error on some tickers: https://github.com/ranaroussi/yfinance/issues/208
 
-        df = data.history(period="max")['Close']   #Download data. Only store the 'Close price' column
-        #print(df)
+        df = data.history(period="1max")['Close']   #Download data. Only store the 'Close price' column
 
-        if (len(df)==0): return     #Ignore empty dataframe (possibly delisted company) 
+        if (len(df)==0): return()     #Ignore empty dataframe (possibly delisted company) 
         
         df.to_sql(ticker, conn, if_exists='replace', index=True)
 
 
 
 
-Indexes = ['DAX', 'MDAX', 'IBEX 35']
 
 #Connect to database (create if file doesn't exist)
 workDir = os.getcwd()
 conn = sqlite3.connect(workDir + '/FinancialData/StockDB.db')
-c = conn.cursor()
+cur = conn.cursor()
 
 for index in Indexes:
     DownloadIndexHistoricData(index)
--- FinancialData/GetTickersListFromIndex.py
+++ FinancialData/GetTickersListFromIndex.py
@@ -5,32 +5,38 @@ import pandas as pd
 #It does so by scraping the index's page on Yahoo Finance, which contains a table of companies with a 'Symbol' column.
 
 
+yahooConversion = {
+    'DAX':'DAXI',
+    'MDAX':'MDAXI',
+    'IBEX':'IBEX'
+}
+
 
 #Given an index, return a list of tickers of all companies in that index
 def GetTickersListFromIndex(index):
 
-    #Store each table from Wikipedia article in a list of dataframes
+    #Store each html table from the page in a list of dataframes
     url = GetIndexUrl(index)
     tables = pd.read_html(url)
     
     #The html page may have many tables. We only want the one containing the ticker symbols
     #This line gets the table index (i) and the column name that contains the symbols
-    (i,colName) = FindTickersColumn(tables)
+    #(i,colName) = FindTickersColumn(tables)
+    (i, colName) = (0, 'Symbol') #Yahoo Finance always uses same html format so we can hardcode these values
 
-    #We now know the table and column name containing the tickers. Grab this column's values and return it as a list
-    return((tables[i][colName].values))
+    #We now know the table and column name containing the tickers. Grab this column's values as a list
+    tickersList = (tables[i][colName].values).tolist()
+
+    #Remove tickers that have no data and cause warnings
+    for delisted in ['SAZ.DE', 'MEO.DE']:
+       if delisted in tickersList: tickersList.remove(delisted)
+
+    return(tickersList)
 
 
 
-IndexUrlCode = {
-    'DAX' : 'GDAXI',
-    'MDAX' : 'MDAXI',
-    'IBEX 35' : 'IBEX'
-}
-
-def GetIndexUrl(indexName):
-    yahooIndex = IndexUrlCode[indexName]    #Yahoo doesn't use the exact nomenclature in the URL, so we must manually correct it for each index (e.g. IBEX 35 -> IBEX)
-    url = 'https://finance.yahoo.com/quote/%5E' + yahooIndex + '/components?p=%5E' + yahooIndex
+def GetIndexUrl(indexName):  
+    url = 'https://finance.yahoo.com/quote/' + indexName + '/components?p=' + indexName
     return(url)
 
 
@@ -45,4 +51,4 @@ def FindTickersColumn(tables):
             if('symbol' in goodColName): return(i,colName)    #Found the Symbol/Ticker column -> Return index of table in webpage, and name of column
 
     #Nothing found -> Return warning
-    print('WARNING: No tickers table found in Wikipedia article')
+    print('WARNING: No tickers table found in page')
